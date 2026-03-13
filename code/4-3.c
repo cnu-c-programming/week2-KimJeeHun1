d7cf10e6e450ba095b63081cbe38cd120590c8bd
KimJeeHun1 <iopuioyui@naver.com>
2026-03-13 10:27:55 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -5,6 +5,10 @@ int main()
     int in;
     scanf("%d", &in);
     int is=1;
+    if(in==1){
+        is=0;
+        goto k;
+            }
     for(int i=2; i<=in/2; i++){
         if(in%i==0)
         {
@@ -12,6 +16,7 @@ int main()
             break;
         }
     }
+    k:
     if(is==1)
     printf("true");
     else
