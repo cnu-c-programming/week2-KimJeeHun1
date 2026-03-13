fac2805116f5d3e1713ffe499ee1c8fbd19a7a99
KimJeeHun1 <iopuioyui@naver.com>
2026-03-13 09:33:31 +0900

    Adjust formatting in printf statements

--- code/1-2.c
+++ code/1-2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("% 5s % 10s %10s\n","name", "id", "value");
-    printf("% 5s %010d % 10.2f\n", "kim", 12, 4.120000);
-    printf("% 5s %010d % 10.2f\n", "lee", 1922, 3.210000);
-    printf("% 5s %010d % 10.2f\n", "park", 432, 1.000000);
+    printf("% 10s % 10s %10s\n","name", "id", "value");
+    printf("% 10s %010d % 10.2f\n", "kim", 12, 4.120000);
+    printf("% 10s %010d % 10.2f\n", "lee", 1922, 3.210000);
+    printf("% 10s %010d % 10.2f\n", "park", 432, 1.000000);
     return 0;
 }
