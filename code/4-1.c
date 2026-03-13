84cb51b99c681e07c5fc70e4a0f9c17432c52b26
KimJeeHun1 <iopuioyui@naver.com>
2026-03-13 10:07:04 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,23 @@
 
 int main()
 {
-
+    int a,b;
+    char c;
+    int oper;
+    scanf("%d %d %c", &a,&b,&c);
+    switch (c){
+        case '+':
+         printf("%d",a+b);
+         break;
+        case '-':
+         printf("%d",a-b);
+         break;
+        case '*':
+         printf("%d",a*b);
+         break;
+        case '/':
+         printf(%d",a/b);
+    }
     return 0;
 }
 
