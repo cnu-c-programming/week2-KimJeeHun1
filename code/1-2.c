2f5c4c3cedecb4d486b089b05d514a10beb115bb
KimJeeHun1 <iopuioyui@naver.com>
2026-03-13 09:32:41 +0900

    Correct printf formatting for output
    Fix formatting issues in printf statements for output alignment.

--- code/1-2.c
+++ code/1-2.c
@@ -2,7 +2,8 @@
 int main()
 {
     printf("% 5s % 10s %10s\n","name", "id", "value");
-    printf("% 5s %010d %f
-
+    printf("% 5s %010d % 10.2f\n", "kim", 12, 4.120000);
+    printf("% 5s %010d % 10.2f\n", "lee", 1922, 3.210000);
+    printf("% 5s %010d % 10.2f\n", "park", 432, 1.000000);
     return 0;
 }
