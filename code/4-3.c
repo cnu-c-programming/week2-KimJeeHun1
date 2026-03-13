14b32621e5be8aec4fb89288236ba527a45350f4
KimJeeHun1 <iopuioyui@naver.com>
2026-03-13 10:12:45 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-
+    int in;
+    scanf("%d", &in);
+    boolean is=true;
+    for(int i=2; i<=in/2; i++){
+        if(in%i==0)
+        {
+            is = false;
+            break;
+        }
+    }
+    printf("%d",is);
     return 0;
 }
 
